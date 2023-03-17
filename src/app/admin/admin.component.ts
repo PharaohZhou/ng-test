d2a7b95cbdcd02ce84739660245c247772c58141
子木 <zl15551633987@163.com>
2023-03-17 08:16:30 +0800

    add material

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewChild } from '@angular/core';
+import {MatSidenav} from "@angular/material/sidenav";
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent {
+  showFiller = false;
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
